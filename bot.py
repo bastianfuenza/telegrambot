1953565edfe8cbb76e1bacbc029467e934399810
Bastian Fuenza <bastian.fuenza.m@gmail.com>
2021-08-15 16:37:52 -0400

    jojobot update

--- bot.py
+++ bot.py
@@ -5,6 +5,18 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 
 from bot_info import BOT_TOKEN
 
+import logging
+
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
+
 # Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -12,43 +24,101 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+FIRST, SECOND = range(2)
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
+
+def start(update: Update, context: CallbackContext) -> int:
+    """Starts the conversation and asks the user about their gender."""
+    reply_keyboard = [['Yes', 'Not Yet']]
+
+    update.message.reply_text(
+        """Hi! My name is PanConBot. I'm a JoJo lover. \n\n"""
+        """Send /cancel to stop talking to me UwU.\n\n"""
+        """Have you watched JoJo's?""",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard,
+            one_time_keyboard=True,
+            input_field_placeholder='Yes or Not Yet?'),
     )
 
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    return FIRST
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def watch(update: Update, context: CallbackContext) -> int:
+    """Stores the selected watch response and asks if wants to watch."""
+    reply_keyboard = [['Yes', 'Of course!']]
+
+    user = update.message.from_user
+    recieved_message = update.message.text
+    logger.info("Has watched? of %s: %s", user.first_name, recieved_message)
+    if recieved_message == 'Yes':
+        reply_message = 'Awesome! Do you want to watch it again?'
+    elif recieved_message == 'Not Yet':
+        reply_message = """It's OK, Do you want to watch it?"""
+
+    update.message.reply_text(reply_message,
+                              reply_markup=ReplyKeyboardMarkup(
+                                  reply_keyboard,
+                                  one_time_keyboard=True,
+                                  input_field_placeholder='Si?',
+                              ))
+
+    return SECOND
+
+
+def info(update: Update, context: CallbackContext) -> int:
+    """Gives info on where to watch to watch."""
+
+    user = update.message.from_user
+    recieved_message = update.message.text
+    logger.info("Will watch? of %s: %s", user.first_name, recieved_message)
+    reply_message = """Great!
+
+    You can watch parts 1-4 on:
+    https://www.netflix.com/cl/title/80179831
+
+    And part 5 on:
+    https://www.crunchyroll.com/es/jojos-bizarre-adventure
+    or just google it :v
+
+    ENJOY!
+    """
+    update.message.reply_text(reply_message,
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
+
+def cancel(update: Update, context: CallbackContext) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
 
 
 def main() -> None:
-    """Start the bot."""
+    """Run the bot."""
     # Create the Updater and pass it your bot's token.
     updater = Updater(BOT_TOKEN)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            FIRST: [MessageHandler(Filters.regex('^(Yes|Not Yet)$'), watch)],
+            SECOND:
+            [MessageHandler(Filters.regex('^(Yes|Of course!)$'), info)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
 
-    # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(
-        MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(conv_handler)
 
     # Start the Bot
     updater.start_polling()
